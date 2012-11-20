7d78869af689ad3964878feeb84f82a45af54cf1
tomek-rusilko <tomek.rusilko@gmail.com>
2012-11-20 21:53:18 +0100

    Finished first implementation of events and participants

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,33 @@
+jQuery(function() {
+
+  // Calendar
+  $('form').on('click', 'input[name*="date"]', function(event) {
+    $(this).datepicker({
+        dateFormat: "yy-mm-dd"
+    }).focus();
+  });
+
+  // Adding and removing Participants
+
+  // Add Participant row
+  $('form').on('click', '.add_participant_btn', function(event) {
+    time = new Date().getTime();
+    regexp = new RegExp($(this).data('id'), 'g');
+    // Add participant association row
+    console.log($(this).data('fields'));
+    //$(this).siblings('table:last').find('tr:last').after($(this).data('fields').replace(regexp, time));
+    $(this).siblings('table:last').find('tbody').append($(this).data('fields').replace(regexp, time));
+    event.preventDefault();
+  });
+
+ // Remove Participant row
+  $('form').on('click', '.remove_event_participation_btn', function(event) {
+    // mark destroy field as 1
+    console.log($(this));
+    $(this).prev('input[type=hidden]').val('1');
+    // hide ci row
+    $(this).closest('tr').hide();
+    event.preventDefault();
+  }); 
+
+});
---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,25 @@
+module EventsHelper
+  def link_to_add_participant_row(name, f, association) 
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.simple_fields_for(association, new_object, child_index: id) do |builder|
+      # new_participant = Participant.new
+      # builder.simple_fields_for :participant, new_participant, defaults: { 
+      #                                         label: false, 
+      #                                         wrapper_html: { class: '' }, 
+      #                                         input_html: { class:'' }
+      #                                       } do |participant_builder|
+        render 'participant_fields', f: builder, i: raw("<i class=\"icon-star new_row_star\"></i>"), aev: f.object.id, new_obj: true
+      # end
+    end
+    link_to(name, '#', class: "btn btn-success add_participant_btn", data: {id: id, fields: fields.gsub("\n", "")})
+  end
+
+  def setup_event(event)
+    unless event.errors.any?
+      2.times { event.event_participations.build.build_participant }
+    end
+    event
+  end
+
+end
