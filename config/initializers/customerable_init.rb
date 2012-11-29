0786a56d8e46d670a97f8d38257adb52043f8637
tomek-rusilko <tomek.rusilko@gmail.com>
2012-11-29 01:35:52 +0100

    fixed assets problem

--- config/initializers/customerable_init.rb
+++ config/initializers/customerable_init.rb
@@ -1,7 +1,7 @@
-require 'customerable.rb'
-# ActiveRecord::Base.send :extend, Customerable
-class ActiveRecord::Base
-  def self.acts_as_customer
-    include Customerable
-  end
-end
+# require 'customerable.rb'
+# # ActiveRecord::Base.send :extend, Customerable
+# class ActiveRecord::Base
+#   def self.acts_as_customer
+#     include Customerable
+#   end
+# end
